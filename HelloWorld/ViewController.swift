9f8ad3a1f5ad884038c4ae5bf26bb3ba4b531462
vincapetrut <vincapetrut@gmail.com>
2022-06-09 14:58:13 +0300

    Added initial commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Petruț Vinca on 08.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello World!")
+    }
+}
